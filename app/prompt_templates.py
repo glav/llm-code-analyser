6df49d9c02cdbd973f0a422b2a7bdc0958d07ed1
Paul Glavich <paulglavich@microsoft.com>
2024-10-15 05:09:39 +0000

    Prompt changes and more structured output

--- app/prompt_templates.py
+++ app/prompt_templates.py
@@ -38,13 +38,11 @@ SYSTEM_PROMPTS = {
     ),
     "final_summary": (
         """You are a helpful assistant who provides a summary of a code based application. You have good knowledge of coding in the java and python languages, SQL query syntax and scripting, as well as a good understanding of the Azure and AWS platform.
-        As a summarisation assistant, you only provide a final summary of content with the following sections ONLY: application purpose, application overall Complexity, and external dependencies as a application Dependency list based on the provided context.
+        As a summarisation assistant, you provide a final summary of content with the following sections ONLY: application purpose, application overall Complexity, and external dependencies.
+        The external dependencies should be presented as an application dependency list based on the provided context.
         The summary should be concise and focus solely on the content presented without adding any additional items or details.
-        Do not include any recommendations or observations as part of the summary.
-        Ensure that you aggregate the complexity scores for all files into 1 score as an average score.
-        Similarly, for purpose and dependencies, aggregate the purpose into one overarching purpose for the application  and for dependencies, list the entire set of dependencies that were found for all files, as a single list of all dependencies,
-        not a summary of each file in the application.
-        Your response should be in the following format:
+        Ensure you do not include recommendations, or potential improvements as part of the summary.
+        Your response to the user query should always be in the following format and not deviate from this format:
 
         Application purpose: [purpose]
         Application Overall Complexity: [complexity score]
@@ -54,9 +52,7 @@ SYSTEM_PROMPTS = {
         Application name: Replatform Java Webapp on AWS
         Application Purpose: This application involves re-platforming a Java-based web application to AWS, utilizing various AWS services and configurations to enhance scalability and reliability.
         Application Overall Complexity: 6 (out of 10)
-        Application Dependency list: AWS Lambda, S3 storage, MySQL database, AWS RDS, AWS EC2, AWS CloudFormation, AWS VPC, Amazon Cognito, Redis storage, Maven plugins, MySQL JDBC Driver.
-
-        Ensure your response does not deviate from the format given above."""
+        Application Dependency list: AWS Lambda, S3 storage, MySQL database, AWS RDS, AWS EC2, AWS CloudFormation, AWS VPC, Amazon Cognito, Redis storage, Maven plugins, MySQL JDBC Driver."""
     ),
 }
 
@@ -65,6 +61,12 @@ USER_PROMPTS = {
     "code": """I want to understand what this code is doing and how complex this code is. Please list any dependencies that the solution has. If you do not detect any dependencies, simply state 'None'
         If you cannot find any code, then do not provide any response. Can you ensure you are as succint as possible in your response and provide the information in the format provided in the system prompt? """,
     "markdown": "Can you summarise the content in 1-2 sentences maximum. Do not exceed 2 sentences and keep the summary as concise as possible. Ensure that the summary is relevant to the context provided. ",
-    "final_summary": """Can you please provide a summary of the entire application based the context provided and be as concise as possible. Your summary should include the purpose of the solution, average complexity of the entire solution,
-    and list any dependencies for the entire application. Do not add any additional items or details.""",
+    "final_summary": """Can you please provide a summary of the entire application based on the context provided. Be as concise as possible. Please do not provide
+        any opinion on the quality of the application or the code quality.
+        Please make sure you do not provide a code analysis summary for each file. You should provide a single summary paragraph for the entire application.
+        When creating the summary for complexity score, you must aggregate the complexity scores for all files into 1 score as an average score.
+        When summarising the application purpose you must summarise each files purpose into one single combined summarised purpose for the application.
+        When summarising application dependencies, please list the entire set of dependencies that were found for all files, as a single list of all dependencies, not a summary of each file in the application.
+        If files do not have any dependencies, then do not list or include them.
+        At the end of the summary, can you list all the dependencies that the application uses in a single concise list""",
 }
--- app/result_store.py
+++ app/result_store.py
@@ -5,14 +5,13 @@ import aiofiles
 
 
 class ResultStore:
-    def __init__(self, results_dir: str = "./_results/"):
+    def __init__(self, results_dir: str = "./_results/", descriptive_suffix: str = ""):
         self.results_dir: str = (
             results_dir if results_dir.endswith("/") else results_dir + "/"
         )
         self.result_name_suffix: str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        self.result_file_name = (
-            f"{self.results_dir}results-{self.result_name_suffix}.txt"
-        )
+        self.descriptive_suffix: str = f"{descriptive_suffix}_" if not None else ""
+        self.result_file_name = f"{self.results_dir}results_{self.descriptive_suffix}{self.result_name_suffix}.txt"
 
     async def add_result_to_store_async(self, result: str):
         await self._ensure_results_dir_exists_async()
